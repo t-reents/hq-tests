0bb0e77b458263b4af77ce39baca88c13a3bb820
Marnik Bercx <mbercx@gmail.com>
2023-09-10 01:26:13 +0300

    👌 Consistent directory naming

--- qe/submit_hq_parallel.sh
+++ qe/submit_hq_parallel.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=_scheduler-stderr.txt
 #SBATCH --partition=debug
 #SBATCH --account=project_465000106
-#SBATCH --nodes=4
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:30:00
@@ -19,11 +19,8 @@ echo "Total Number of tasks: $SLURM_NTASKS"
 echo "CPUs on this Node: $SLURM_CPUS_ON_NODE"
 
 /pfs/lustrep3/users/mabercx/bin/hq worker start --no-hyper-threading &
-sleep 1
 /pfs/lustrep3/users/mabercx/bin/hq worker start --no-hyper-threading &
-sleep 1
 /pfs/lustrep3/users/mabercx/bin/hq worker start --no-hyper-threading &
-sleep 1
-/pfs/lustrep3/users/mabercx/bin/hq worker start --no-hyper-threading &
+# /pfs/lustrep3/users/mabercx/bin/hq worker start --no-hyper-threading &
 
 wait
