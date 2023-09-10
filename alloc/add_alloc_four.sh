4cc857ff025f0212d50b5a581ffe1ef4e83c46fd
Marnik Bercx <mbercx@gmail.com>
2023-09-10 19:22:13 +0300

    ✨ Add HQ allocation scripts

---
+++ alloc/add_alloc_four.sh
@@ -0,0 +1,1 @@
+hq alloc add slurm --time-limit 30m --backlog 1 --workers-per-alloc 4 --max-worker-count 4 --name test --cpus 128 -- -p debug -A project_465000106 --mem=227318
---
+++ alloc/add_alloc_single.sh
@@ -0,0 +1,1 @@
+hq alloc add slurm --time-limit 30m --backlog 1 --workers-per-alloc 1 --max-worker-count 1 --name single --cpus 128 -- -p debug -A project_465000106 --mem=227318 --ntasks-per-node=128
